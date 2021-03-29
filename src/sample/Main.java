d04096e41565460bb6021ad67727ad0430a7de1c
neya-java <sergey.arlashkin@gmail.com>
2021-03-29 11:24:35 +0300

    Create homework lvl2 lesson4

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("MyChat");
-        primaryStage.setScene(new Scene(root, 400, 800));
+        primaryStage.setScene(new Scene(root, 401, 800));
         primaryStage.show();
         
     }
